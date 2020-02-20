0fee6f32bea8e95e7590778195d7d72ccbce1c6b
Voloreft <val_saint@mail.ru>
2020-02-20 16:46:16 +0300

    Initial commit

---
+++ val.py
@@ -0,0 +1,48 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def intro():
+    return 'Миссия Колонизация Марса'
+
+
+@app.route('/index')
+def index():
+    return 'И на Марсе будут яблони цвести!'
+
+
+@app.route('/promotion')
+def promo():
+    return '''Человечество вырастает из детства.
+    <br>
+    Человечеству мала одна планета.
+    <br>
+    Мы сделаем обитаемыми безжизненные пока планеты.
+    <br>
+    И начнем с Марса!
+    <br>
+    Присоединяйся!'''
+
+
+@app.route('/image_mars')
+def image():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="/static/flask/mars.jpg">
+                  </body>
+                </html>"""
+
+
+#
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
